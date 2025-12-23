44cdcadefa1211f6738ed3bff18ee8c2f88ebd6b
felipe1496 <felipe.oliveira1496@gmail.com>
2025-12-23 10:05:57 -0300

    fix: order by entries problem

--- resources/transactions/handler.go
+++ resources/transactions/handler.go
@@ -115,7 +115,7 @@ func (api *API) ListViewEntries(ctx *gin.Context) {
 	page := ctx.GetInt("page")
 	perPage := ctx.GetInt("per_page")
 	filter := ctx.MustGet("filter").(*utils.FilterBuilder).And("user_id", "eq", userID).
-		And("period", "eq", period).OrderBy("created_at", "desc")
+		And("period", "eq", period).OrderBy("reference_date", "desc")
 
 	entries, err := api.transactionsUseCase.ListViewEntries(filter)
 
